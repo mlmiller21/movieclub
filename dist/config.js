a42a188f85c0d3391cdc2c32a5a284d0b2b2614d
mmiller21 <m_miller21@outlook.com>
2021-01-15 16:58:26 -0500

    ignore ./dist and ./node_modules

--- dist/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dbConfig = void 0;
-exports.dbConfig = {
-    database: "movieclub",
-    username: "postgres",
-    password: "e327pk9m",
-};
-//# sourceMappingURL=config.js.map
--- dist/typeormconfig.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dbConfig = void 0;
-exports.dbConfig = {
-    database: "movieclub",
-    username: "postgres",
-    password: "e327pk8m",
-};
-//# sourceMappingURL=typeormconfig.js.map
